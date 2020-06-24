93c0000b0bbe25f3909305c5a1009c037614b7e1
Mosen <mosen@users.noreply.github.com>
2020-06-24 16:25:22 +1000

    PROJQUAY-740: Remove generated stubs as there are way too many and they cause tons of mess in the repo. Switched to ignoring untyped modules for now, to concentrate on Quay-only type checking. PROJQUAY-740: mypy config changed to ignore missing imports PROJQUAY-740: ignore property decorator as it is not supported by mypy

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+from typing import Optional, Dict, Any
 from uuid import uuid4
 
 import os.path
@@ -187,8 +188,8 @@ class DefaultConfig(ImmutableConfig):
     MAIL_SERVER = ""
     MAIL_USE_TLS = True
     MAIL_PORT = 587
-    MAIL_USERNAME = None
-    MAIL_PASSWORD = None
+    MAIL_USERNAME = None  # type: Optional[str]
+    MAIL_PASSWORD = None  # type: Optional[str]
     MAIL_DEFAULT_SENDER = "example@projectquay.io"
     MAIL_FAIL_SILENTLY = False
     TESTING = True
@@ -245,11 +246,11 @@ class DefaultConfig(ImmutableConfig):
     SENTRY_PUBLIC_DSN = None
 
     # Github Config
-    GITHUB_LOGIN_CONFIG = None
+    GITHUB_LOGIN_CONFIG = None  # type: Optional[Dict[str, Any]]
     GITHUB_TRIGGER_CONFIG = None
 
     # Google Config.
-    GOOGLE_LOGIN_CONFIG = None
+    GOOGLE_LOGIN_CONFIG = None  # type: Optional[Dict[str, Any]]
 
     # Bitbucket Config.
     BITBUCKET_TRIGGER_CONFIG = None
@@ -383,7 +384,7 @@ class DefaultConfig(ImmutableConfig):
     # user's repository list pages, regardless of whether that user is a member of the namespace.
     # Typically, this is used by an enterprise customer in configuring a set of "well-known"
     # namespaces.
-    PUBLIC_NAMESPACES = []
+    PUBLIC_NAMESPACES = []  # type: List[str]
 
     # The namespace to use for library repositories.
     # Note: This must remain 'library' until Docker removes their hard-coded namespace for libraries.
@@ -441,7 +442,7 @@ class DefaultConfig(ImmutableConfig):
         "logo": "/static/img/quay-horizontal-color.svg",
         "footer_img": None,
         "footer_url": None,
-    }
+    }  # type: Dict[str,Optional[str]]
 
     # How often the Garbage Collection worker runs.
     GARBAGE_COLLECTION_FREQUENCY = 30  # seconds
@@ -457,7 +458,7 @@ class DefaultConfig(ImmutableConfig):
     SECURITY_SCANNER_ENDPOINT = "http://192.168.99.101:6060"
 
     # The endpoint for the V4 security scanner.
-    SECURITY_SCANNER_V4_ENDPOINT = None
+    SECURITY_SCANNER_V4_ENDPOINT = None  # type: Optional[str]
 
     # The number of seconds between indexing intervals in the security scanner
     SECURITY_SCANNER_INDEXING_INTERVAL = 30
@@ -503,7 +504,7 @@ class DefaultConfig(ImmutableConfig):
     REPO_MIRROR_TLS_VERIFY = True
 
     # Replaces the SERVER_HOSTNAME as the destination for mirroring.
-    REPO_MIRROR_SERVER_HOSTNAME = None
+    REPO_MIRROR_SERVER_HOSTNAME = None  # type: Optional[str]
 
     # JWTProxy Settings
     # The address (sans schema) to proxy outgoing requests through the jwtproxy
@@ -568,8 +569,8 @@ class DefaultConfig(ImmutableConfig):
 
     # Site key and secret key for using recaptcha.
     FEATURE_RECAPTCHA = False
-    RECAPTCHA_SITE_KEY = None
-    RECAPTCHA_SECRET_KEY = None
+    RECAPTCHA_SITE_KEY = None  # type: Optional[str]
+    RECAPTCHA_SECRET_KEY = None  # type: Optional[str]
 
     # Server where TUF metadata can be found
     TUF_SERVER = None
@@ -622,18 +623,18 @@ class DefaultConfig(ImmutableConfig):
     BROWSER_API_CALLS_XHR_ONLY = True
 
     # If set to a non-None integer value, the default number of maximum builds for a namespace.
-    DEFAULT_NAMESPACE_MAXIMUM_BUILD_COUNT = None
+    DEFAULT_NAMESPACE_MAXIMUM_BUILD_COUNT = None  # type: Optional[int]
 
     # If set to a non-None integer value, the default number of maximum builds for a namespace whose
     # creator IP is deemed a threat.
-    THREAT_NAMESPACE_MAXIMUM_BUILD_COUNT = None
+    THREAT_NAMESPACE_MAXIMUM_BUILD_COUNT = None  # type: Optional[int]
 
     # The API Key to use when requesting IP information.
     IP_DATA_API_KEY = None
 
     # For Billing Support Only: The number of allowed builds on a namespace that has been billed
     # successfully.
-    BILLED_NAMESPACE_MAXIMUM_BUILD_COUNT = None
+    BILLED_NAMESPACE_MAXIMUM_BUILD_COUNT = None  # type: Optional[int]
 
     # Configuration for the data model cache.
     DATA_MODEL_CACHE_CONFIG = {
