fc1d4395646551e8ac8b6471e182cebf8a267209
Mosen <mosen@users.noreply.github.com>
2020-06-24 20:03:19 +1000

    PROJQUAY-740: add more missing annotations PROJQUAY-740: ignore type checking for things like monkey patching and exported/re-imported objects that mypy does not allow.

--- data/database.py
+++ data/database.py
@@ -17,7 +17,7 @@ import toposort
 
 from enum import IntEnum, Enum, unique
 from peewee import *
-from peewee import __exception_wrapper__, Function
+from peewee import __exception_wrapper__, Function  # type: ignore
 from playhouse.pool import PooledMySQLDatabase, PooledPostgresqlDatabase, PooledSqliteDatabase
 
 from sqlalchemy.engine.url import make_url
