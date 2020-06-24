54cdcdf0c73c2677d8ad0abb1b1126d3acc7b2f5
Mosen <mosen@users.noreply.github.com>
2020-06-24 14:32:33 +1000

    PROJQUAY-740: Add dev dependencies mypy and typing PROJQUAY-740: Add makefile target `types-test`, not yet included in `test` target. PROJQUAY-740: Minor typing additions.

--- test/testconfig.py
+++ test/testconfig.py
@@ -1,5 +1,6 @@
 import os
 
+from typing import List
 from datetime import datetime, timedelta
 from tempfile import NamedTemporaryFile
 
@@ -111,4 +112,4 @@ class TestConfig(DefaultConfig):
 
     FEATURE_REPO_MIRROR = True
     FEATURE_GENERAL_OCI_SUPPORT = True
-    OCI_NAMESPACE_WHITELIST = []
+    OCI_NAMESPACE_WHITELIST = []  # type: List[str]
